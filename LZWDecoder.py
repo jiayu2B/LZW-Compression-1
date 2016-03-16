695ad14d685d06c127d8e509c475fd29a659e74e
sumanttapas <stapas@uncc.edu>
2016-03-15 20:43:55 -0500

    Added files via upload

---
+++ LZWDecoder.py
@@ -0,0 +1,66 @@
+﻿import sqlite3
+from sys import argv
+import struct
+args = str(argv)
+
+conn = sqlite3.connect(':memory:')  # Declare a SQLITE object with a database in the RAM
+code_Value = 255  # initial value of a new code generated
+MAX_TABLE_VALUE = pow(2, int(argv[2]))
+
+
+# Function to initialize the dictionary to ascii values 0 to 255
+def db_init():
+    conn.execute('''CREATE TABLE dict (
+        code INTEGER,
+        stringVal TEXT)''')
+    for k in range(0, 255):
+        conn.execute("insert into dict (code,stringVal) values (?,?)", (str(k), chr(k)))
+
+db_init()  # Initialize the dictionary
+inputList = []  #
+inputFile = open('G:\\UNCC\\Spring 16\\Algos\\Project\\LZW\\' + argv[1], "rb")
+code = inputFile.read()
+
+stri = '>'
+print(len(code))
+for i in range(int(len(code)/2)):
+    stri += 'H'
+
+inputList = struct.unpack(stri, code)
+print(inputList)
+index = 0
+
+
+query = "select stringVal from dict where code = "+str(inputList[0])
+rdr = conn.execute("select stringVal from dict where code = ?", (inputList[0],))  # conn.execute(query)
+data = (rdr.fetchone())
+string = str(data[0])
+outputstring = string
+
+for i in range(1, len(inputList)):
+    code = inputList[i]
+    #query = "select stringVal from dict where code = "+str(code)
+    rdr = conn.execute("select stringVal from dict where code = ?", (code,))  # conn.execute(query)
+    data = (rdr.fetchone())
+    if(data == None):
+        newString = string+string[:1]
+    else:
+        newString = str(data[0])
+    print(newString)
+    outputstring = outputstring+newString
+    query = "select count(*) from dict"
+    rdr = conn.execute(query)
+    noOfEntries = rdr.fetchone()
+    if(int(noOfEntries[0]) < MAX_TABLE_VALUE):
+        code_Value = code_Value + 1 
+        # query = "insert into dict (code, stringVal) values ("+str(code_Value)+",'"+string+newString[:1]+"')"
+        conn.execute("insert into dict (code,stringVal) values (?,?)", (code_Value, string+newString[:1]))
+        # conn.execute(query)
+    string = newString
+inputFile.close()
+outputfile = open('G:\\UNCC\\Spring 16\\Algos\\Project\\LZW\\' + argv[1][:-4]+'_Decoded.txt', "w")
+outputfile.write(outputstring)
+outputfile.close()
+print(outputstring)
+
+conn.close()
---
+++ LZWEncoder.py
@@ -0,0 +1,78 @@
+﻿import sqlite3
+from sys import argv
+import struct
+
+
+conn = sqlite3.connect(':memory:')
+code_Value = 255
+intOutputString = []
+index = 0
+
+
+def db_init(cur):
+    cur.execute('''CREATE TABLE dict (
+        code INTEGER,
+        stringVal TEXT)''')
+    for i in range(0, 255):
+        conn.execute("insert into dict (code,stringVal) values (?,?)", (str(i), chr(i)))
+
+
+cur = conn.cursor()
+db_init(cur)
+
+string = None
+
+
+MAX_TABLE_VALUE = pow(2, int(argv[2]))
+filename = "G:\\UNCC\\Spring 16\\Algos\\Project\\LZW\\" + argv[1]
+fileobj = open(filename, "r")
+while True:
+    c = fileobj.read(1)
+    if(not c):
+        break
+    else:
+        symbol = c
+        if string != None:
+            param = string+str(symbol)
+        else:
+            param = str(symbol)
+        query = "select count(*) from dict where stringVal = '"+param+"'"
+        rdr = conn.execute("select count(*) from dict where stringVal = ?", (param,))
+        count = rdr.fetchone()
+        if int(count[0]) > 0:
+            string = param
+        else:
+            query = "select code from dict where stringVal = '"+string+"'"
+            # rdr = conn.execute(query)
+            rdr = conn.execute("select code from dict where stringVal = ?", (string,))
+            codeVal = rdr.fetchone()
+            intOutputString.insert(index, int(codeVal[0]))
+            print(int(codeVal[0]))
+            query = "select count(*) from dict"
+            rdr = conn.execute(query)
+            noOfEntries = rdr.fetchone()
+            if int(noOfEntries[0]) < MAX_TABLE_VALUE:
+                code_Value += 1
+                conn.execute("insert into dict (code,stringVal) values (?,?)", (str(code_Value), param))
+            string = symbol
+    index += 1
+query = "select code from dict where stringVal = '"+string+"'"
+codeVal1 = conn.execute("select code from dict where stringVal = ?", (string,))# conn.execute(query)
+
+final = codeVal1.fetchone()
+print(int(final[0]))
+intOutputString.insert(index, int(final[0]))
+print(intOutputString)
+
+
+outputFileName = "G:\\UNCC\\Spring 16\\Algos\\Project\\LZW\\"+argv[1][:-3]+"lzw"
+outputfile = open(outputFileName, "wb")
+
+for i in range(len(intOutputString)):
+    outputfile.write(struct.pack('>H', intOutputString[i]))
+
+
+outputfile.close()
+fileobj.close()
+cur.close()
+#conn.close()
